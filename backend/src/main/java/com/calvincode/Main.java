944243c924d4baa83dfaea2b5b296f900df94f20
Calvin <wfleung@ucsd.edu>
2023-10-14 15:20:30 -0400

    added new linw and removed unused import

--- backend/src/main/java/com/calvincode/Main.java
+++ backend/src/main/java/com/calvincode/Main.java
@@ -8,9 +8,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 @SpringBootApplication()
@@ -19,7 +16,9 @@ public class Main {
 
 
     public static void main(String[] args) {
+
         SpringApplication.run(Main.class, args);
+
     }
 
     @Bean
